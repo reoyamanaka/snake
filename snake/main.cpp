24086c0f8ae3e31e2d4c8ad82491ac3419930ce1
Reo Yamanaka <ryamanaka807@gmail.com>
2020-10-24 02:40:51 -0400

    refactoring code

--- snake/main.cpp
+++ snake/main.cpp
@@ -1,63 +1,53 @@
 #include <iostream>
 using namespace std;
 
-//global variables
-bool gameOver;
-const int width = 20;
-const int height = 20;
-int x, y, fruitX, fruitY, score;
-enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
-eDirection dir;
+#include <Windows.h>
 
-void Setup() {
-	gameOver = false;
-	dir = STOP;
-	x = width / 2;
-	y = height / 2;
-	fruitX = rand() % width;
-	fruitY = rand() % height;
-	score = 0;
+int nScreenWidth = 120;
+int nScreenHeight = 80;
 
-}
-void Draw() {
-	system("cls");
-	for (int i = 0; i < width+2; i++) {
-		cout << "#";	
-	}
-	cout << endl;
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
-			if (j == 0) {
-				cout << "#";
-				cout << "                    ";
-			}
-			if (j == width - 1) {
-				cout << "#";
-			}
-		}
-		cout << endl;
-	}
-	for (int i = 0; i < width+2; i++) {
-		cout << "#";
-	}
-}
-
-void Input() {
-
-}
-
-void Logic() {
-
-}
+struct sSnakeSegment {
+	int x;
+	int y;
+};
 
 int main() {
-	Setup();
+	//create screen buffer
+	wchar_t* screen = new wchar_t[nScreenWidth * nScreenHeight];
+	for (int i = 0; i < nScreenWidth * nScreenHeight; i++) screen[i] = L' ';
+		HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(hConsole);
+	DWORD dwBytesWritten = 0;
 
-	while (!gameOver) {
-		Draw();
-		Input();
-		Logic();
+	//creating linked list object for snake
+	list<sSnakeSegment> snake = { {60,15}, {61, 15},{62, 15}, {63, 15}, {64, 15}, {65, 15}, {66, 15}, {67, 15}, {68, 15}, {69, 15}, };  //snake's default state - for each element of the snake, we'll store some coordinates. we're starting at segment {60, 15};
+	int nFoodX = 30;
+	int nFoodY = 15;
+	int nScore = 0;
+	int nSnakeDirection = 3;
+	bool bDead = false;
+
+
+
+	
+	while (1) {
+		//timing and input
+
+
+
+		//game logic
+
+		//display to player
+		//clear screen
+		for (int i = 0; i < nScreenWidth * nScreenHeight; i++) screen[i] = L' ';
+
+		//draw stats and border
+
 		
+		//display frame
+		WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth, nScreenHeight, { 0, 0 }, &dwBytesWritten);
+
 	}
+
 	return 0;
 }
